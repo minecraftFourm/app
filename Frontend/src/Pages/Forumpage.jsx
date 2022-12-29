5271808f3750bc51cce0f9dc7a42ef3c48c0f6a4
Benedict <benedict.arowo@gmail.com>
2022-12-29 17:40:47 +0300

    Changed forum header into a component

--- Frontend/src/Pages/Forumpage.jsx
+++ Frontend/src/Pages/Forumpage.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import ForumBg from "../assets/fourmbg.jfif"
+import ForumHeader from "../Components/ForumHeader";
 
 const Forumpage = () => {
   return (
     <div className="pb-32 bg-[#1B263B] ">
       {/* Hero */}
-      <section className="h-96 w-full relative">
-        <img src={ForumBg} alt="Fourm Background" className="h-full w-full" />
-        <div className={`absolute top-0 z-0 bottom-0 left-0 right-0 grid place-items-center text-white bg-[#00000080] duration-1000`}>
-          <h1 className={`font-extrabold text-6xl duration-700 opacity-100 z-10`}>Server Forum</h1>
-        </div>
-      </section>
+      <ForumHeader />
 
       {/* Forum */}
       <div className="w-full h-[1200px] pt-16 flex px-16 py-4 gap-8">

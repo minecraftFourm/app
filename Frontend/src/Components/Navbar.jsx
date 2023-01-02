53b00a794b1db854073a516cacad60ba3030abec
Benedict <benedict.arowo@gmail.com>
2023-01-02 13:36:24 +0300

    + Profile picture support

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -24,6 +24,7 @@ const Navbar = () => {
     }
   }
   
+  console.log(user)
   return (
     <>    
       <div className="grid h-16 bg-white drop-shadow-lg w-full" id="header">
@@ -58,7 +59,7 @@ const Navbar = () => {
           <div className="md:hidden">
             {user.isAuthenticated ? (
               <div className="flex flex-row items-center gap-2">
-                <img src="https://s3-alpha-sig.figma.com/img/2bf0/38b2/24b6fd21fbe783bc14937744f3e9a9cc?Expires=1672617600&Signature=NvublJObKVhmjMBxLObHUbRHEagUU~qUA3lyoSCgO1otPB~h5IFEU125~tAh~48DYX4LOdYFwTzVSBk724hreypVr1R1P6DOgDMpWQUiYswZ9g5V93slEbdE6aalP3d0AwrL-svT8xXmfI6EkDaEu6THoUUN3p8ZCeg5pLiR07CPvHxmU8KVONl-ssv-LqDEpXODba8-I3nHJIUf-1KTNZNvf~FuaKbTyAJzscPKeq6zypywTo-kpcoW0npW7r5y6SSsDEOzRa1-bbrzWny5lWVQMJbDbLxs65p0jJotsYBd9B70mo368I-4CjB7S9NPjFEMBYiciu7p~Sx21AwIIQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" className="w-[48px] h-[48px] object-cover object-center rounded-full " alt="" />
+                <img src={user.profilePicture} className="w-[48px] h-[48px] object-cover object-center rounded-full " alt="" />
                 <button className="mr-4 text-sm font-medium" onClick={handleLogout}>Logout</button>
               </div>
             ) : (

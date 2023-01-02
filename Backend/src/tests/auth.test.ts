b6f7e29107930a4bda553a51d9553e513d519d57
Bryan Avila <bryanjimavila@gmail.com>
2023-01-02 11:47:07 +0800

    add register route test

--- Backend/src/tests/auth.test.ts
+++ Backend/src/tests/auth.test.ts
@@ -15,6 +15,11 @@ describe('auth test suite', ()=>{
     })
     
     describe('/login test', () =>{
+        afterEach(() =>{
+            jest.clearAllMocks()
+            jest.resetAllMocks()
+        })
+
         const userData = {
             id: "101e15a6-bcd7-4c73-a7b6-1b1d11f44781",
             password: "testing123",
@@ -129,7 +134,6 @@ describe('auth test suite', ()=>{
             .expect('Content-Type', /json/)
             .expect(400)
 
-            console.log(argonMock.mock.results[0])
             expect(res.body.err).toBe('Invalid Password')
         })
 
@@ -148,5 +152,149 @@ describe('auth test suite', ()=>{
             expect(prisma.user.findUnique).toBeCalled()
             expect(res.body.err).toBe('DB Error')
         })
+    })
+
+    describe('/register test', () =>{
+        afterEach(() =>{
+            jest.clearAllMocks()
+            jest.resetAllMocks()
+        })
+
+
+        const userData = {
+            id: "101e15a6-bcd7-4c73-a7b6-1b1d11f44781",
+            password: "testing123",
+            invalid_pass: "pass",
+            email: 'testing@gmail.com',
+            invalid_email: "zxcasd.com",
+            username: "testuser",
+            invalid_user: "test",
+        }
+
+        
+        const jwtMock = jest.spyOn(jwt, 'sign')
+        const argonMock = jest.spyOn(argon, 'verify')
+        it('should call register router', async () =>{
+            const {email,username, password} = userData
+            prisma.user.create = jest.fn().mockReturnValue({
+                "id": "101e15a6-bcd7-4c73-a7b6-1b1d11f44781",
+                username,
+                email,
+                password
+            })
+            argonMock.mockImplementationOnce(async (x, y) =>{
+                return x==y
+            })
+
+            await request(app)
+            .post('/register')
+            .send({username, email, password})
+            .expect('Content-Type', /json/)
+            .expect(201)
+
+            expect(prisma.user.create).toBeCalled()
+            expect(jwtMock).toHaveBeenCalled()
+        })
+
+        it('should return user values after success registration', async () =>{
+            const {email, password, username, id} = userData
+            prisma.user.create = jest.fn().mockReturnValue({
+                "id": "101e15a6-bcd7-4c73-a7b6-1b1d11f44781",
+                username,
+                email,
+                password
+            })
+            argonMock.mockImplementationOnce(async (x, y) =>{
+                return x==y
+            })
+            
+
+            const res = await request(app)
+            .post('/register')
+            .send({email, username, password})
+            .expect('Content-Type', /json/)
+            .expect(201)
+
+            expect(res.body).toEqual({id, email, username})
+            expect(prisma.user.create).toHaveBeenCalled()
+
+            expect(jwtMock).toHaveBeenCalled()
+
+        })
+
+        it('should return error 400 when invalid username was entered', async () =>{
+            const {invalid_user, username, password, email} = userData
+            prisma.user.create = jest.fn().mockReturnValue({
+                "id": "101e15a6-bcd7-4c73-a7b6-1b1d11f44781",
+                username,
+                email,
+                password
+            })
+
+
+            const res = await request(app)
+            .post('/register')
+            .send({email, username: invalid_user, password})
+            .expect('Content-Type', /json/)
+            .expect(400)
+
+            expect(res.body.err).toBe("Invalid Username.")
+        })
+
+        it('should return error 400 when invalid password was entered', async () =>{
+            const {email, username, password, invalid_pass} = userData
+            prisma.user.create = jest.fn().mockReturnValue({
+                "id": "101e15a6-bcd7-4c73-a7b6-1b1d11f44781",
+                username,
+                email,
+                password
+            })
+
+            const res = await request(app)
+            .post('/register')
+            .send({email, username, password: invalid_pass})
+            .expect('Content-Type', /json/)
+            .expect(400)
+
+            expect(res.body.err).toBe('Invalid Password.')
+        })
+
+
+        it('should return error 400 when invalid email was entered', async () =>{
+            const {email, username, password, invalid_email} = userData
+            prisma.user.create = jest.fn().mockReturnValue({
+                "id": "101e15a6-bcd7-4c73-a7b6-1b1d11f44781",
+                username,
+                email,
+                password
+            })
+
+            const res = await request(app)
+            .post('/register')
+            .send({email: invalid_email, username, password})
+            .expect('Content-Type', /json/)
+            .expect(400)
+
+            expect(res.body.err).toBe('Invalid Email.')
+        })
+
+
+
+        it('should return error 500 if DB Fails', async () =>{
+            const {email, password} = userData
+            prisma.user.findUnique = jest.fn().mockImplementation(() =>{
+                throw new Error('DB Error')
+            })
+            
+            const res = await request(app)
+            .post('/login')
+            .send({email, password})
+            .expect('Content-Type', /json/)
+            .expect(500)
+
+            expect(prisma.user.findUnique).toBeCalled()
+            expect(res.body.err).toBe('DB Error')
+        })
+
     })
 })

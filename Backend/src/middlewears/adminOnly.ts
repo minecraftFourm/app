912b651e13d0103b844aacd34090a8b013cb4ab9
Bryan Avila <bryanjimavila@gmail.com>
2023-01-03 19:54:52 +0800

    update admin verification middleware

--- Backend/src/middlewears/adminOnly.ts
+++ Backend/src/middlewears/adminOnly.ts
@@ -1,10 +1,11 @@
+import { Role } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import CustomError from "./custom-error";
 
 interface Req extends Request {
     user?: {
-        role: string;
+        role: Role;
     }
 } 
 
@@ -12,7 +13,7 @@ interface Req extends Request {
 const adminOnly = (req: Req, res : Response, next: NextFunction) => {
     const user = req.user;
     
-    if (!user || user.role.toUpperCase() != "USER") throw new CustomError(ReasonPhrases.UNAUTHORIZED, StatusCodes.UNAUTHORIZED)
+    if (!user || !user.role.isAdmin) throw new CustomError(ReasonPhrases.UNAUTHORIZED, StatusCodes.UNAUTHORIZED)
 
     // ! If the user's role is not admin, then they can't access the content...
     next();

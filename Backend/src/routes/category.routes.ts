eff04a23e6b315f7fa39eb3a45ae4681a90a97d3
Bryan Avila <bryanjimavila@gmail.com>
2023-01-04 17:07:41 +0800

    add adminOnly middleware

--- Backend/src/routes/category.routes.ts
+++ Backend/src/routes/category.routes.ts
@@ -1,16 +1,17 @@
+import { Router } from "express";
 import { createCategory, deleteCategory, editCategory, getCategories, getCategory } from "../controllers/category.controller";
+import { adminOnly } from "../middlewears/adminOnly";
 import wrapper from "../middlewears/async-wrapper";
 import auth from "../middlewears/auth";
 
-const { Router } = require('express');
 export const categoryRouter = Router();
 
 
 categoryRouter.route('/')
     .get(wrapper(getCategories))
-    .post(auth, wrapper(createCategory))
+    .post(auth, adminOnly, wrapper(createCategory))
 
 categoryRouter.route('/:id')
-    .delete(auth, wrapper(deleteCategory))
+    .delete(auth, adminOnly, wrapper(deleteCategory))
     .get(wrapper(getCategory))
-    .patch(auth, wrapper(editCategory))
+    .patch(auth, adminOnly, wrapper(editCategory))

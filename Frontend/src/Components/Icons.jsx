721593ede7ae368ac063a82774e879ed71df1a9f
Benedict <benedict.arowo@gmail.com>
2023-01-20 14:34:06 +0300

    Added Icons component, and html to text package

---
+++ Frontend/src/Components/Icons.jsx
@@ -0,0 +1,20 @@
+export const LoadingIcon = (props) => {
+    const { color = 'white', width = 12, height = 12 } = props;
+    return (
+        <div className="w-full grid place-content-center min-h-[120px]">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-${width} h-${height} spin text-${color}`}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+            </svg>
+        </div>
+    )
+}
+
+
+export const AddNewIcon = (props) => {
+    const { style, width = 12, height = 12 } = props;
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-${width} h-${height} ${style}`}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+        </svg>
+    )
+}

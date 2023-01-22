ffeb0541b7acb5427f555d7f85b17e8ba024546f
Benedict <benedict.arowo@gmail.com>
2023-01-22 18:33:50 +0300

    Made minor changes to the colors, and typography.

--- Frontend/src/Pages/NotFoundPage.jsx
+++ Frontend/src/Pages/NotFoundPage.jsx
@@ -7,12 +7,13 @@ const NotFoundPage = () => {
   return (
     <div className='mt-6 pt-6'>
       <div className="grid grid-cols-1">
-       <div className='row-span-1 mx-auto'><img className='object-contain h-25 w-25' src={image404} alt='404' /></div>
-              <div className='row-span-1 mx-auto text-gray-500'><b>Oooops......</b></div>
+       <div className='row-span-1 mx-auto'>
+              <img className='object-contain h-25 w-25' src={image404} alt='404' /></div>
+              <div className='row-span-1 mx-auto text-gray-500 text-3xl'><b>Oooops......</b></div>
               <div className='row-span-1 mx-auto text-gray-400'>The page you are looking for doesn't seem to exist.</div>
             <div className='row-span-1 mx-auto mt-3'>
             <Link to="/">
-              <button style={{ backgroundColor: "#7F7EFF" }} className="hover:bg-blue-700 text-white py-1 px-6 border border-blue-700 rounded">
+              <button className="hover:bg-indigo-700 bg-indigo-500 text-white py-1 px-6 border border-indigo-600 transition-colors duration-300 rounded">
               Go Home
               </button>
             </Link>

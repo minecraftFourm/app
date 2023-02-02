219fc72ec687a7b77385065403cabb175da02303
Benedict <benedict.arowo@gmail.com>
2023-02-02 21:28:37 +0000

    Styling fixes
    Added a min-height for the post box, and the user info box beside it.
    Tailwind acts weird with colors when being used as a variable, so I've changed it to using pure css for the role background color.
    Some padding, and margin fixes as well.

--- Frontend/src/Pages/ViewPost.jsx
+++ Frontend/src/Pages/ViewPost.jsx
@@ -51,21 +51,22 @@ const ViewPost = () => {
         </div>
       )}
       {!isLoading && !err && (
-        <div className="bg-white h-fit pb-2 px-4 mx-6 md:mx-2 sm:mx-0 sm:px-2 mt-16 pt-4">
-          <div className="w-full h-fit outline outline-1 outline-gray-400">
+        <div className="bg-white h-fit mx-6 md:mx-2 sm:mx-0 sm:px-2 mt-16 p-4">
+          <div className="w-full h-fit outline outline-1 outline-gray-400 pb-4">
             <p className="w-full bg-violet-500 text-white text-xl px-2 py-2 drop-shadow-lg">
               {post.title}
             </p>
-            <div className="px-2 h-fit flex flex-row gap-2 mx-1 mt-4 mb-2">
-              <div className="w-[400px] min-w-fit min-h-full mb-4 pb-2 outline outline-1 outline-gray-400">
+            <div className="px-2 flex flex-row gap-2 mx-1 mt-4 min-h-[500px]">
+              <div className="max-w-[300px] w-full pb-2 outline outline-1 outline-gray-400">
                 <div className="mb-4 h-full">
                     <img className='mx-auto pt-6' src={post.owner.profilePicture} alt={`profile picture of ${post.owner.username}`}></img>
-                    <div className="w-full h-fit flex flex-col flex-wrap items-center justify-center">
-                      <p className="text-3xl font-bold">{post.owner.username}</p>
-                      <div className={`bg-[${post.owner.role.color}]`}>
-                        <p className="bg-indigo-400 text-sm text-white font-bold rounded-sm border-[1px] px-2 outline-indigo-700">{post.owner.role.title.toUpperCase()}</p>
+                    <div className="w-full h-fit flex flex-col flex-wrap items-center justify-center mt-2">
+                      <p className="text-3xl font-bold capitalize">{post.owner.username}</p>
+                      
+                      <div>
+                        <p style={{ backgroundColor: post.owner.role.color}} className="text-sm text-white font-bold rounded-sm border-[1px] px-2 outline-indigo-700">{post.owner.role.title.toUpperCase()}</p>
                       </div>
-                      <div className="w-full h-full">
+                      <div className="w-full h-full mt-4 px-4">
                         <div className="px-2 w-full flex flex-row justify-between items-center">
                             <p className="font-bold text-gray-500">Joined:</p>
                             <p>{format(post.owner.created)}</p>
@@ -82,11 +83,12 @@ const ViewPost = () => {
                     </div>
                   </div>
               </div>
-              <div className="w-full min-h-full max-h-fit mb-4 px-2 py-2 outline outline-1 outline-gray-400 bg-gray-100">
+              <div className="w-full px-2 py-2 outline outline-1 flex flex-col justify-between outline-gray-400 bg-gray-100">
               <div
                 className="pb-4"
                 dangerouslySetInnerHTML={{ __html: post.content }}
-              ></div>
+              >
+              </div>
               <p className="text-gray-500 text-sm">{format(post.updated)}</p>
               </div>
             </div>

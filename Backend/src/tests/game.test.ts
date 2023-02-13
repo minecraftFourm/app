5089147c0273987eecd9e247c8db906edd12999d
Bryan <bryanjimavila@gmail.com>
2023-02-13 21:04:10 +0800

    add new tests

--- Backend/src/tests/game.test.ts
+++ Backend/src/tests/game.test.ts
@@ -3,13 +3,13 @@ import {app} from '../app'
 import prisma from '../db/prisma.client'
 
 
-describe.only('auth test suite', ()=>{
+describe('game test suite', ()=>{
     
     const userData = {
         id: "4a6de4ef-3f5e-46a0-b409-aa2cd2532509",
-        password: "testpass444",
-        email: 'test444@gmail.com',
-        username: "testuser444"
+        password: "adminuser",
+        email: 'admin@example.com',
+        username: "admin"
     }
 
     const gameData1 = {
@@ -37,7 +37,7 @@ describe.only('auth test suite', ()=>{
 
     let response: request.Response
 
-    beforeEach( async () =>{
+    beforeAll( async () =>{
         const {email, password} = userData
 
         const res = await request(app)
@@ -65,8 +65,6 @@ describe.only('auth test suite', ()=>{
         .expect(200)
 
         expect(prisma.game.findMany).toHaveBeenCalled()
-        // find the cookie in the header
-        console.log(res.body)
     })
 
     it('should return a single game', async () =>{
@@ -80,8 +78,6 @@ describe.only('auth test suite', ()=>{
         .expect(200)
 
         expect(prisma.game.findUnique).toHaveBeenCalled()
-        // find the cookie in the header
-        console.log(res.body)
     })
 
     it('should register/add a game', async () =>{
@@ -98,11 +94,9 @@ describe.only('auth test suite', ()=>{
         .expect(200)
 
         expect(prisma.game.create).toHaveBeenCalled()
-        // find the cookie in the header
-        console.log(res.body)
     })
 
-    it.only('should delete an existing game', async () =>{
+    it('should delete an existing game', async () =>{
         let gameId = gamesData[0].id
         prisma.game.delete = jest.fn().mockReturnValueOnce(gamesData.filter(v => v.id !== gameId))
 
@@ -113,7 +107,61 @@ describe.only('auth test suite', ()=>{
         .expect(200)
 
         expect(prisma.game.delete).toHaveBeenCalled()
-        // find the cookie in the header
-        console.log(res.body)
+    })
+
+    it('should return 401 if user is not logged in', async () =>{
+        const res = await request(app)
+        .post('/game')
+        .send(gameData1)
+        .expect('Content-Type', /json/)
+        .expect(401)
+    })
+
+    it('should return 404 if a game was not found', async () =>{
+        prisma.game.findUnique = jest.fn().mockImplementationOnce((v: string) => {
+            const data = gamesData.filter(gd => gd.id === v)
+            return data.length === 0 && false
+        })
+
+        
+        const res = await request(app)
+        .get('/game/4a6de4ef-3f5e-46a0-b409-aa2cd2532509')
+        .set("Cookie", [...response.header['set-cookie']])
+        .expect(404)
+    
+
+        expect(prisma.game.findUnique).toBeCalled()
+        expect(res.body.err).toBe('Game not found')
+    })
+
+    it('should not register/add a game if title field is missing', async () =>{
+        prisma.game.create = jest.fn().mockReturnValue({
+            "id": "101e15a6-bcd7-4c73-a7b6-1b1d11f44781",
+            ...gameData1
+        })
+
+        const res = await request(app)
+        .post('/game')
+        .set("Cookie", [...response.header['set-cookie']])
+        .send({
+            title: ""
+        })
+        .expect('Content-Type', /json/)
+        .expect(400)
+
+        expect(prisma.game.create).toHaveBeenCalledTimes(0)
+    })
+
+    it('should return error 500 if DB Fails', async () =>{
+        prisma.game.findMany = jest.fn().mockImplementation(() =>{
+            throw new Error('DB Error')
+        })
+        
+        const res = await request(app)
+        .get('/game')
+        .set("Cookie", [...response.header['set-cookie']])
+
+        expect(prisma.game.findMany).toBeCalled()
+        expect(res.body.err).toBe('DB Error')
     })
 })

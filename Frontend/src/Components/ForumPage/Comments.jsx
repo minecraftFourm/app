648228d7737a80e75533a7620fd99b7a12cf5161
Benedict <benedict.arowo@gmail.com>
2023-02-27 16:22:31 +0300

    + Deleting comments, and posts

--- Frontend/src/Components/ForumPage/Comments.jsx
+++ Frontend/src/Components/ForumPage/Comments.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
 import { format } from "timeago.js";
+import { useFetch } from "../../Contexts/Fetch";
+import { UseUser } from "../../Contexts/UserContext";
 
 const Comments = (props) => {
-	const { comments } = props;
+	const { comments, reloadComments } = props;
+	const { role: UserRole, isAuthenticated, id: CurrentUserID } = UseUser();
+	const CustomFetch = useFetch();
+
+	const deleteComment = async (commentId) => {
+		const DeleteComment = CustomFetch({
+			url: `comment/${commentId}`,
+			options: {
+				method: "DELETE",
+			},
+			returnResponse: true,
+		});
+
+		toast.promise(DeleteComment, {
+			loading: "Deleting comment...",
+			success: ({ data, response }) => {
+				if (!response.ok) {
+					throw new Error();
+				}
+
+				reloadComments();
+				return "Sucessfully deleted your comment!";
+			},
+			error: (err) => {
+				console.log(err);
+				return "An error occured while deleting your comment!";
+			},
+		});
+	};
 
 	const Component = () => {
 		return comments.map((comment) => {
@@ -21,7 +52,7 @@ const Comments = (props) => {
 					id: userId,
 				},
 			} = comment;
-
+			console.log(comment);
 			return (
 				<div
 					key={id}
@@ -78,19 +109,26 @@ const Comments = (props) => {
 							<p className="text-sm">{format(updated)}</p>
 
 							<div className="flex flex-row gap-3">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth={1.5}
-									stroke="currentColor"
-									className="w-6 h-6">
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-									/>
-								</svg>
+								{isAuthenticated &&
+									(UserRole.isAdmin ||
+										UserRole.canDeleteOtherPost ||
+										(UserRole.canDeletePost &&
+											CurrentUserID === userId)) && (
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth={1.5}
+											stroke="currentColor"
+											className="w-6 h-6 cursor-pointer"
+											onClick={() => deleteComment(id)}>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+											/>
+										</svg>
+									)}
 
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
--- Frontend/src/Pages/ViewPost.jsx
+++ Frontend/src/Pages/ViewPost.jsx
@@ -1,12 +1,13 @@
 import DOMPurify from "dompurify";
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import ForumHeader from "../Components/ForumHeader";
 import { useFetch } from "../Contexts/Fetch";
 import { LoadingIcon } from "../Components/Icons";
 import { format } from "timeago.js";
 import Comments from "../Components/ForumPage/Comments";
 import { UseUser } from "../Contexts/UserContext";
+import { toast } from "react-hot-toast";
 
 const ViewPost = () => {
 	const { id } = useParams();
@@ -14,7 +15,8 @@ const ViewPost = () => {
 	const [post, setPost] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 	const [err, setErr] = useState(null);
-	const User = UseUser();
+	const { role: UserRole, isAuthenticated, id: CurrentUserID } = UseUser();
+	const Navigate = useNavigate();
 
 	useEffect(() => {
 		(async () => {
@@ -35,11 +37,53 @@ const ViewPost = () => {
 		})();
 	}, []);
 
-	console.log(post);
+	const deletePost = async (postId, categoryId) => {
+		const DeletePost = CustomFetch({
+			url: `post/${postId}`,
+			options: {
+				method: "DELETE",
+			},
+			returnResponse: true,
+		});
+
+		toast.promise(DeletePost, {
+			loading: "Deleting post...",
+			success: ({ data, response }) => {
+				if (!response.ok) {
+					throw new Error();
+				}
+				Navigate(`/forum/category/${categoryId}`, {
+					replace: true,
+				});
+				return "Sucessfully deleted your post!";
+			},
+			error: (err) => {
+				console.log(err);
+				return "An error occured while deleting your post!";
+			},
+		});
+	};
+
+	const fetchPostDetails = async () => {
+		try {
+			const { data, response } = await CustomFetch({
+				url: `post/${id}`,
+				returnResponse: true,
+			});
+			if (!response.ok) throw Error();
+			setPost(data.data);
+		} catch (error) {
+			console.log(error);
+			setErr(true);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
 	return (
 		<div className="bg-[#1B263B] pb-16">
 			<ForumHeader />
+			<button onClick={fetchPostDetails}>Click me</button>
 			{isLoading && (
 				<div className="mt-4">
 					<LoadingIcon />
@@ -118,19 +162,32 @@ const ViewPost = () => {
 										{format(post.updated)}
 									</p>
 									<div className="flex flex-row gap-3">
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											strokeWidth={1.5}
-											stroke="currentColor"
-											className="w-6 h-6">
-											<path
-												strokeLinecap="round"
-												strokeLinejoin="round"
-												d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-											/>
-										</svg>
+										{isAuthenticated &&
+											(UserRole.isAdmin ||
+												UserRole.canDeleteOtherPost ||
+												(UserRole.canDeletePost &&
+													CurrentUserID ===
+														post.ownerId)) && (
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 24 24"
+													strokeWidth={1.5}
+													stroke="currentColor"
+													className="w-6 h-6"
+													onClick={() =>
+														deletePost(
+															post.id,
+															post.categoryId
+														)
+													}>
+													<path
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+													/>
+												</svg>
+											)}
 
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
@@ -167,7 +224,7 @@ const ViewPost = () => {
 						<div className="mt-8 px-2">
 							<div className="w-full flex flex-row justify-between gap-2 items-center px-2 text-violet-500">
 								<h4 className="text-2xl font-bold">Comments</h4>
-								{User.isAuthenticated && (
+								{isAuthenticated && (
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
 										fill="none"
@@ -185,7 +242,10 @@ const ViewPost = () => {
 							</div>
 
 							<div>
-								<Comments comments={post.comments} />
+								<Comments
+									comments={post.comments}
+									reloadComments={fetchPostDetails}
+								/>
 							</div>
 						</div>
 					</div>

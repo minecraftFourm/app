80634eab2c4bd059ede409dfbf0cc4abaac0a65d
Benedict <benedict.arowo@gmail.com>
2023-03-30 07:13:19 +0300

    Minor fixes

--- Frontend/src/Components/HomePage/Announcement.jsx
+++ Frontend/src/Components/HomePage/Announcement.jsx
@@ -7,7 +7,6 @@ const Announcement = ({ ...props }) => {
 	const Items = props.items;
 	const Component = () => {
 		return Items.map((item) => {
-			console.log(item);
 			const {
 				id,
 				title,

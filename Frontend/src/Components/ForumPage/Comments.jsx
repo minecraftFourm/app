cd9655a5099ba9882e76d8d09020ca372133ef53
Benedict <benedict.arowo@gmail.com>
2023-04-05 23:24:42 +0300

    Added edit comment function

--- Frontend/src/Components/ForumPage/Comments.jsx
+++ Frontend/src/Components/ForumPage/Comments.jsx
@@ -49,6 +49,10 @@ const Comments = (props) => {
 		});
 	};
 
+	const handleEdit = ({ id, content }) => {
+		console.log(id, content);
+	};
+
 	const Component = () => {
 		return comments.map((comment) => {
 			const {
@@ -155,7 +159,13 @@ const Comments = (props) => {
 											viewBox="0 0 24 24"
 											strokeWidth={1.5}
 											stroke="currentColor"
-											className="w-6 h-6 cursor-pointer">
+											className="w-6 h-6 cursor-pointer"
+											onClick={() =>
+												handleEdit({
+													id,
+													content: commentContent,
+												})
+											}>
 											<path
 												strokeLinecap="round"
 												strokeLinejoin="round"
@@ -178,7 +188,6 @@ const Comments = (props) => {
 										d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
 									/>
 								</svg>
-
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
 									fill="none"

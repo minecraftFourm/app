f4a19ef3e2c8799713c469481cc968cfbd3c60c4
Benedict <benedict.arowo@gmail.com>
2023-04-08 14:24:42 +0300

    Minor adjustments

--- Frontend/src/Components/ForumHeader.jsx
+++ Frontend/src/Components/ForumHeader.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import ForumBg from "../assets/fourmbg.jfif"
-import Overlay from './Overlay'
+import React from "react";
+import ForumBg from "../assets/fourmbg.jfif";
+import Overlay from "./Overlay";
 
-const ForumHeader = () => {
-  return (
-    <section className="h-96 w-full relative">
-        <img src={ForumBg} alt="Fourm Background" className="h-full w-full" />
-        <Overlay 
-          title="Server Forum"
-        />
-    </section>
-  )
-}
+const ForumHeader = (props) => {
+	const { title } = props;
+	return (
+		<section className="h-96 w-full relative">
+			<img
+				src={ForumBg}
+				alt="Fourm Background"
+				className="h-full w-full"
+			/>
+			<Overlay title={title ? title : "Server Forum"} />
+		</section>
+	);
+};
 
-export default ForumHeader
+export default ForumHeader;

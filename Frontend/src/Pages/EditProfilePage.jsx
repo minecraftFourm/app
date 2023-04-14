aec637ba8595f3c2cb8a80fbaf289f6ac71b6e1d
Dexterlente <dexter.lente18@gmail.com>
2023-04-14 20:57:42 +0800

    Some changes

--- Frontend/src/Pages/EditProfilePage.jsx
+++ Frontend/src/Pages/EditProfilePage.jsx
@@ -4,40 +4,45 @@ import pretty from '../assets/pretty.png'
 
 const EditProfilePage = () => {
   return (
-    <div className='h-screen'>
-        <img src={bgprofile} alt="bgprofile" className='h-2/5 w-screen' />
+    <div>
+          <div>
+            <img src={bgprofile} alt="bgprofile" className='relative h-[500px] w-screen' />
+            <div className='absolute inset-x-0 top-60'>
+              <img src={pretty} className='mx-auto h-52 w-52 border-black border-2 rounded-full' />
+            </div>
+        </div>
         <div className='grid grid-cols-1 py-3'>
-          <label className='font-bold text-gray-700 text-2xl px-6'>Username:
+          <label className='font-bold text-gray-400 text-2xl px-6'>Username:
             <div>
-              <input className='w-11/12'/>
+              <input type="text" className='w-11/12'/>
             </div>
           </label>
-          <label className='font-bold text-gray-700 text-2xl px-6'>Email:
+          <label className='font-bold text-gray-400 text-2xl px-6'>Email:
             <div>
-              <input className='w-11/12'/>
+              <input type="text" className='w-11/12'/>
             </div>
           </label>
-          <label className='font-bold text-gray-700 text-2xl px-6'>Bio:
+          <label className='font-bold text-gray-400 text-2xl px-6'>Bio:
             <div>
-              <input className='w-11/12'/>
+              <textarea className='w-11/12 h-[150px] border-black border-solid border-[1px]'/>
             </div>
           </label>
-          <label className='font-bold text-gray-700 text-2xl px-6'>Instagram:
+          <label className='font-bold text-gray-400 text-2xl px-6'>Instagram:
             <div>
-              <input className='w-11/12'/>
+              <input type="text" className='w-11/12'/>
             </div>
           </label>
-          <label className='font-bold text-gray-700 text-2xl px-6'>Discord:
+          <label className='font-bold text-gray-400 text-2xl px-6'>Discord:
             <div>
-              <input className='w-11/12'/>
+              <input type="text" className='w-11/12'/>
             </div>
           </label>
-          <label className='font-bold text-gray-700 text-2xl px-6'>Minecraft Username:
+          <label className='font-bold text-gray-400 text-2xl px-6'>Minecraft Username:
             <div>
-              <input className='w-11/12'/>
+              <input type="text" className='w-11/12'/>
             </div>
           </label>
-          <label className='font-bold text-gray-700 text-2xl px-6'>Show Email:
+          <label className='font-bold text-gray-400 text-2xl px-6'>Show Email:
             <input type="checkbox" className='ml-2 w-6 h-6' />
           </label>
         </div>

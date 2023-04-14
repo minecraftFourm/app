801258e8e6a4052a97370481c89f40e433947300
Dexterlente <dexter.lente18@gmail.com>
2023-04-14 21:10:32 +0800

    Design Done

--- Frontend/src/Pages/EditProfilePage.jsx
+++ Frontend/src/Pages/EditProfilePage.jsx
@@ -12,37 +12,37 @@ const EditProfilePage = () => {
             </div>
         </div>
         <div className='grid grid-cols-1 py-3'>
-          <label className='font-bold text-gray-400 text-2xl px-6'>Username:
+          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Username:
             <div>
-              <input type="text" className='w-11/12'/>
+              <input type="text" className='w-11/12 text-lg font-normal ' placeholder="Benedict"/>
             </div>
           </label>
-          <label className='font-bold text-gray-400 text-2xl px-6'>Email:
+          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Email:
             <div>
-              <input type="text" className='w-11/12'/>
+              <input type="text" className='w-11/12 text-lg font-normal ' placeholder="Benedict"/>
             </div>
           </label>
-          <label className='font-bold text-gray-400 text-2xl px-6'>Bio:
+          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Bio:
             <div>
-              <textarea className='w-11/12 h-[150px] border-black border-solid border-[1px]'/>
+              <textarea className='w-11/12 text-lg font-normal h-[150px] border-black border-solid border-[1px]' placeholder="Benedict"/>
             </div>
           </label>
-          <label className='font-bold text-gray-400 text-2xl px-6'>Instagram:
+          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Instagram:
             <div>
-              <input type="text" className='w-11/12'/>
+              <input type="text" className='w-11/12 text-lg font-normal' placeholder="Benedict"/>
             </div>
           </label>
-          <label className='font-bold text-gray-400 text-2xl px-6'>Discord:
+          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Discord:
             <div>
-              <input type="text" className='w-11/12'/>
+              <input type="text" className='w-11/12 text-lg font-normal' placeholder="Benedict"/>
             </div>
           </label>
-          <label className='font-bold text-gray-400 text-2xl px-6'>Minecraft Username:
+          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Minecraft Username:
             <div>
-              <input type="text" className='w-11/12'/>
+              <input type="text" className='w-11/12 text-lg font-normal' placeholder="Benedict"/>
             </div>
           </label>
-          <label className='font-bold text-gray-400 text-2xl px-6'>Show Email:
+          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Show Email:
             <input type="checkbox" className='ml-2 w-6 h-6' />
           </label>
         </div>

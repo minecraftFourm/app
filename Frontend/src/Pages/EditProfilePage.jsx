5bd762b0fd58a07ea6f42185cca3b0383ce4c520
Benedict <benedict.arowo@gmail.com>
2023-04-17 13:02:13 +0300

    Added states, fetchRequests, toast

--- Frontend/src/Pages/EditProfilePage.jsx
+++ Frontend/src/Pages/EditProfilePage.jsx
@@ -1,53 +1,234 @@
-import React from 'react'
-import bgprofile from '../assets/bgprofile.png'
-import pretty from '../assets/pretty.png'
+import React, { useEffect, useState } from "react";
+import bgprofile from "../assets/bgprofile.png";
+import pretty from "../assets/pretty.png";
+import { UseUser } from "../Contexts/UserContext";
+import { useFetch } from "../Contexts/Fetch";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 const EditProfilePage = () => {
-  return (
-    <div>
-          <div>
-            <img src={bgprofile} alt="bgprofile" className='relative h-[500px] w-screen' />
-            <div className='absolute inset-x-0 top-60'>
-              <img src={pretty} className='mx-auto h-52 w-52 border-black border-2 rounded-full' />
-            </div>
-        </div>
-        <div className='grid grid-cols-1 py-3'>
-          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Username:
-            <div>
-              <input type="text" className='w-11/12 text-lg font-normal ' placeholder="Benedict"/>
-            </div>
-          </label>
-          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Email:
-            <div>
-              <input type="text" className='w-11/12 text-lg font-normal ' placeholder="Benedict"/>
-            </div>
-          </label>
-          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Bio:
-            <div>
-              <textarea className='w-11/12 text-lg font-normal h-[150px] border-black border-solid border-[1px]' placeholder="Benedict"/>
-            </div>
-          </label>
-          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Instagram:
-            <div>
-              <input type="text" className='w-11/12 text-lg font-normal' placeholder="Benedict"/>
-            </div>
-          </label>
-          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Discord:
-            <div>
-              <input type="text" className='w-11/12 text-lg font-normal' placeholder="Benedict"/>
-            </div>
-          </label>
-          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Minecraft Username:
-            <div>
-              <input type="text" className='w-11/12 text-lg font-normal' placeholder="Benedict"/>
-            </div>
-          </label>
-          <label className='font-bold text-gray-400 text-2xl px-6 my-2'>Show Email:
-            <input type="checkbox" className='ml-2 w-6 h-6' />
-          </label>
-        </div>
-    </div>
-  )
-}
+	const User = UseUser();
+	const CustomFetch = useFetch();
+	const Navigate = useNavigate();
+	const [user, setUser] = useState();
+	const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState({
+		username: "",
+		email: "",
+		bio: "",
+		discord: "",
+		instagram: "",
+		mc_username: "",
+		showMail: "",
+		banner: "",
+		profilePicture: "",
+	});
 
-export default EditProfilePage
+	useEffect(() => {
+		(async () => {
+			const { response, data } = await CustomFetch({
+				url: `user/${User.id}`,
+				returnResponse: true,
+			});
+
+			if (!response.ok) {
+				Navigate("/notfound");
+			}
+			console.log(data.data);
+
+			setData(() => {
+				return {
+					username: data.data.username,
+					email: data.data.email,
+					bio: data.data.bio,
+					discord: data.data.discord,
+					instagram: data.data.instagram,
+					mc_username: data.data.mc_username,
+					showMail: data.data.showMail,
+					profilePicture: data.data.profilePicture,
+					banner: data.data.banner.url,
+				};
+			});
+
+			setUser(data.data);
+			setIsLoading(false);
+		})();
+	}, []);
+
+	const updateData = (newData) => {
+		setData((currentData) => {
+			return {
+				...currentData,
+				...newData,
+			};
+		});
+
+		console.log(data);
+	};
+
+	const Save = () => {
+		const SaveRequest = CustomFetch({
+			url: `user/${User.id}`,
+			options: {
+				body: JSON.stringify(data),
+				method: "PATCH",
+			},
+			returnPromise: true,
+		});
+
+		toast.promise(SaveRequest, {
+			loading: "Saving data...",
+			success: () => {
+				Navigate(`/profile`, {
+					replace: true,
+				});
+				return "Sucessfully saved your profile!";
+			},
+			error: (err) => {
+				console.log(err);
+				return "An error occured save your new profile details!";
+			},
+		});
+	};
+
+	return (
+		<>
+			{!isLoading && (
+				<div className="bg-white">
+					<div className="h-[500px] w-full grid place-content-center relative">
+						<div className="absolute bg-transparent grid place-content-center z-20 top-0 bottom-0 left-0 right-0">
+							<div className="bg-white w-[400px] h-[300px] rounded-sm"></div>
+						</div>
+						<img
+							src={user.banner.url}
+							alt={user.banner.name}
+							className="absolute w-full h-full cursor-pointer object-cover"
+							onClick={() => console.log("Banner")}
+						/>
+						<img
+							src={user.profilePicture}
+							alr={`${user.username}'s profile picture.`}
+							className="mx-auto h-52 w-52 border-black border-2 z-10 cursor-pointer object-cover rounded-full"
+							onClick={() => console.log("Profile Picture")}
+						/>
+					</div>
+					<div className="grid grid-cols-1 py-3">
+						<label className="font-bold text-slate-700 text-xl px-6 my-2">
+							Username:
+							<div>
+								<input
+									type="text"
+									className="px-2 py-1 w-11/12 text-base font-light"
+									placeholder={user.username}
+									value={data.username}
+									onChange={(e) =>
+										updateData({ username: e.target.value })
+									}
+								/>
+							</div>
+						</label>
+						<label className="font-bold text-slate-700 text-xl px-6 my-2">
+							Email:
+							<div>
+								<input
+									type="text"
+									className="px-2 py-1 w-11/12 text-base font-light"
+									placeholder={user.email}
+									value={data.email}
+									onChange={(e) => {
+										updateData({ email: e.target.value });
+									}}
+								/>
+							</div>
+						</label>
+						<label className="font-bold text-slate-700 text-xl px-6 my-2">
+							Bio:
+							<div>
+								<textarea
+									className="px-2 w-11/12 text-base font-light py-1 h-[150px] border-black border-solid border-[1px]"
+									placeholder={user.bio}
+									value={data.bio}
+									onChange={(e) => {
+										updateData({ bio: e.target.value });
+									}}
+								/>
+							</div>
+						</label>
+						<label className="font-bold text-slate-700 text-xl px-6 my-2">
+							Instagram:
+							<div>
+								<input
+									type="text"
+									className="w-11/12 text-base px-2 py-1 font-light"
+									placeholder={user.instagram}
+									value={data.instagram}
+									onChange={(e) => {
+										updateData({
+											instagram: e.target.value,
+										});
+									}}
+								/>
+							</div>
+						</label>
+						<label className="font-bold text-slate-700 text-xl px-6 my-2">
+							Discord:
+							<div>
+								<input
+									type="text"
+									className="w-11/12 text-base px-2 py-1 font-light"
+									placeholder={user.discord}
+									value={data.discord}
+									onChange={(e) => {
+										updateData({ discord: e.target.value });
+									}}
+								/>
+							</div>
+						</label>
+						<label className="font-bold text-slate-700 text-xl px-6 my-2">
+							Minecraft Username:
+							<div>
+								<input
+									type="text"
+									className="w-11/12 text-base px-2 py-1 font-light"
+									placeholder={user.mc_username}
+									value={data.mc_username}
+									onChange={(e) => {
+										updateData({
+											mc_username: e.target.value,
+										});
+									}}
+								/>
+							</div>
+						</label>
+						<label className="font-bold text-slate-700 text-xl px-6 my-2">
+							Show Email:
+							<input
+								type="checkbox"
+								checked={
+									data.showMail == "false" ? false : true
+								}
+								className="ml-2 w-4 h-4"
+								id="checkbox"
+								onChange={(e) => {
+									updateData({
+										showMail: `${e.target.checked}`,
+									});
+								}}
+							/>
+						</label>
+						<div className="justify-center w-full flex">
+							<button
+								className="hover:bg-indigo-700 cursor-pointer bg-indigo-500 text-white py-1 px-6 border border-indigo-600 transition-colors duration-300 rounded"
+								type="submit"
+								onClick={Save}>
+								Save
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default EditProfilePage;
